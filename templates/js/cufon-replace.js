09b8fb1328d12f7c10eb33e77548cf77a395f254
argerim <argerim@hotmail.com>
2011-05-31 18:20:24 -0300

    Incio do css com menu, falta imagens, por Rogerio.

---
+++ templates/js/cufon-replace.js
@@ -0,0 +1,4 @@
+Cufon.replace('#faded li a, header nav li', { fontFamily: 'Myriad Pro Regular', textShadow: '#000 1px 1px', hover:true });
+Cufon.replace('h1', { fontFamily: 'Myriad Pro Regular', textShadow: '#000 1px 1px', color: '-linear-gradient(#830100, #83013B)' });
+Cufon.replace('h1 span', { fontFamily: 'Myriad Pro Regular', color: '-linear-gradient(#000, #000)' });
+
