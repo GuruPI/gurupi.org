f1d4204f02e9347eabb02dcab81adca87e8ecfb6
Cairo Noleto <caironoleto@gmail.com>
2011-06-11 19:38:11 -0300

    rotas para new e create

--- spec/routing/post_routing_spec.rb
+++ spec/routing/post_routing_spec.rb
@@ -5,5 +5,11 @@ describe PostsController do
     it "#show" do
       { get: '/posts/this-is-my-post'}.should route_to(controller: 'posts', action: 'show', id: 'this-is-my-post')
     end
+    it "#new" do
+      { get: '/posts/novo'}.should route_to(controller: 'posts', action: 'new')
+    end
+    it "#create" do
+      { post: '/posts'}.should route_to(controller: 'posts', action: 'create')
+    end
   end
 end
