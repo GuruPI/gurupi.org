e285fb38e4e67a5ff880b42b7e38cbfd89a9a5c6
Cleiton Francisco <cleitonfco@gmail.com>
2011-10-01 08:54:35 -0300

    Removendo teste pendente

--- spec/models/lecture_spec.rb
+++ spec/models/lecture_spec.rb
@@ -7,12 +7,4 @@ describe Lecture do
     lecture.relative_votes = 5
   end
 
-  it "should order lectures from balance" do
-    pending "Como fazer um teste que garanta a ordem dentro de um array?"
-    lecture1 = Lecture.create(:positive_vote => 10, :negative_vote => 1)
-    lecture2 = Lecture.create(:positive_vote => 11, :negative_vote => 2)
-    lecture3 = Lecture.create(:positive_vote => 9,  :negative_vote => 0)
-    Lecture.ranking.should == [lecture3, lecture1, lecture2]
-  end
-
 end
