cb339849255ca0e63f2e6d8992f81de16efa948b
Rogerio Medeiros <argerim@gmail.com>
2012-08-16 14:08:37 -0300

    change test.

--- spec/routing/about_routing_spec.rb
+++ spec/routing/about_routing_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 
 describe "Routing for About page" do
   it "get /about should route to welcome#about " do
-    {:get => 'about'}.should route_to(
+    {:get => 'sobre'}.should route_to(
       :controller => "welcome",
       :action     => "about"
     )
