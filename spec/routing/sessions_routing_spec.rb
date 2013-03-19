998fa0aede1fb605866596414d2894194bfffaed
Ricardo Franco <ricardo.krieg@gmail.com>
2013-03-19 10:46:48 -0300

    Test para o rota create (/auth/github/callback)

--- spec/routing/sessions_routing_spec.rb
+++ spec/routing/sessions_routing_spec.rb
@@ -7,6 +7,10 @@ describe SessionsController do
       get("/auth/facebook/callback").should route_to("sessions#create", :provider => 'facebook')
     end
 
+    it "routes to #create" do
+      get("/auth/github/callback").should route_to("sessions#create", :provider => 'github')
+    end
+
     it "routes to #destroy" do
       get("/signout").should route_to("sessions#destroy")
     end
