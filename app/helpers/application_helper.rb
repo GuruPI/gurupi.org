0e9b7bf466b549163ecedd99fa741a24f11efef1
Cleiton Francisco <cleitonfco@gmail.com>
2013-03-27 07:29:56 -0300

    Helper: Page Title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 # encoding: UTF-8
 module ApplicationHelper
+  def page_title
+    default = "GuruPI - Grupo de Usuários de Ruby do Piauí"
+    title = @title.blank? ? default : "#{@title} - #{default}"
+    title
+  end
+
   def user_role(user)
     pattern = %{<span class="user_role %s">%s</span>}
     type = (user.admin? ? ["admin", "Admin"] : (user.member? ? ["member", "Membro"] : ["guest", "Visitante"]))
