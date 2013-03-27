164e06dd1cef62106b90a39719314e230d1d42cd
Cleiton Francisco <cleitonfco@gmail.com>
2013-03-27 07:30:22 -0300

    Helper: Analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,20 @@ module ApplicationHelper
     title
   end
 
+  def analytics_code(params = {})
+    code = %{
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-39555893-1', 'gurupi.org');
+  ga('send', 'pageview');
+</script>}
+    code.html_safe
+  end
+
   def user_role(user)
     pattern = %{<span class="user_role %s">%s</span>}
     type = (user.admin? ? ["admin", "Admin"] : (user.member? ? ["member", "Membro"] : ["guest", "Visitante"]))
