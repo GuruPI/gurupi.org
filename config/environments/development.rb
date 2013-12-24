f8fdd9d569dd6beaf541080975056c4fc67e945a
Cleiton Francisco <cleitonfco@gmail.com>
2013-12-24 01:37:32 -0300

    Assets Pipeline

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,5 +21,21 @@ GurupiOrg::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-end
 
+  # Do not compress assets
+  # config.assets.compress = false
+  # config.serve_static_assets = true
+  # config.assets.compress = true
+  # config.assets.compile = false
+  # config.assets.precompile = [ Proc.new{ |path| !File.extname(path).in?(['.js', '.css', '']) }, /(?:\/|\\|\A)application\.(css|js)$/ ]
+
+# Disable Rails's static asset server (Apache or nginx will already do this)
+config.serve_static_assets = false
+# Compress JavaScripts and CSS
+config.assets.compress = true
+# Fallback to assets pipeline if a precompiled asset is missed
+config.assets.compile = true
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
+end
