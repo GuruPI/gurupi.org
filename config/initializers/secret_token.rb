a246b12a7664eb95ca86c3e161d29e14ccff2dcf
Dmitry Rocha <dmitryrck@gmail.com>
2016-10-24 18:57:03 -0300

    Run rake rails:update task.
    This task would have been run in b10cbb75511e4318f038fa2c170ca90212206df6 commit.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-GurupiOrg::Application.config.secret_token = 'deb188f0820dea1f71b3837a9ce7c35450204c1fa65331088f474992b79d7fee3c58b3b4ab3b5bd770ab598d007c564b441d73bf7e4a9b4c6a4dd3d16335c1de'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GurupiOrg::Application.config.secret_key_base = 'bb2969357e4c2b96dbc4d8c2bb0ecdc1130fc3167a5a8cd398b9ab5f167b7a794171bcc59877618644d0185bf819a010c5ef80e33311f37602b0985ba9a69fc7'
